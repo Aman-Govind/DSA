61972bb3b38caf6552ad975848358bdd43972e54
Aman Govind <amangovind48@gmail.com>
2026-08-08 09:55:59 +0530

    Time: 2 ms (51.25%) | Memory: 19.4 MB (18.51%) - LeetSync

---
+++ 443-string-compression/string-compression.py
@@ -0,0 +1,27 @@
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        if len(chars)==1:
+            return 1
+        res=[]
+        stack=[chars[0]]
+        for i in chars[1:]:
+            if stack[-1]==i:
+                stack.append(i)
+            else:
+                char=stack[0]
+                res.append(char)
+                if len(stack)>1 and len(stack)<10:
+                    res.append(str(len(stack)))
+                elif len(stack)>=10:
+                    for j in str(len(stack)):
+                        res.append(j)
+                stack=[i]
+        if stack:
+            ch=stack[0]
+            res.append(ch)
+            if len(stack)>1 and len(stack)<10:
+                res.append(str(len(stack)))
+            elif len(stack)>=10:
+                for j in (str(len(stack))):
+                    res.append(j)
+        chars[:]=res
