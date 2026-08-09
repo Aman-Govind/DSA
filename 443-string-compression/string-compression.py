55fa164ad575e3fe617c71577344f39fdd9ea148
Aman Govind <amangovind48@gmail.com>
2026-08-09 21:18:03 +0530

    Time: 3 ms (43.91%) | Memory: 19.2 MB (87.33%) - LeetSync

--- 443-string-compression/string-compression.py
+++ 443-string-compression/string-compression.py
@@ -10,17 +10,11 @@ class Solution:
             else:
                 char=stack[0]
                 res.append(char)
-                if len(stack)>1 and len(stack)<10:
-                    res.append(str(len(stack)))
-                elif len(stack)>=10:
-                    for j in str(len(stack)):
-                        res.append(j)
+                if len(stack)>1:
+                    res+=list(str(len(stack)))
                 stack=[i]
         ch=stack[0]
         res.append(ch)
-        if len(stack)>1 and len(stack)<10:
-            res.append(str(len(stack)))
-        elif len(stack)>=10:
-            for j in (str(len(stack))):
-                res.append(j)
+        if len(stack)>1:
+            res+=list(str(len(stack)))
         chars[:]=res
