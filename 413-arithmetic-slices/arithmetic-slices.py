5d9448a4b0b1f68b6c9266c325b4f5e2b7b10b7f
Aman Govind <amangovind48@gmail.com>
2026-08-26 11:12:19 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (85.96%) - LeetSync

---
+++ 413-arithmetic-slices/arithmetic-slices.py
@@ -0,0 +1,11 @@
+class Solution:
+    def numberOfArithmeticSlices(self, nums: List[int]) -> int:
+        ans=0
+        cur=0
+        for i in range(2,len(nums)):
+            if nums[i]-nums[i-1]==nums[i-1]-nums[i-2]:
+                cur+=1
+                ans+=cur
+            else:
+                cur=0
+        return ans
